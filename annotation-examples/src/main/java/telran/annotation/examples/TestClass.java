824eef4330d64d3e33f08ef76ecccabfaa7fda43
Vera Kuznetsova <kuznetsova-vera@yandex.ru>
2023-10-29 18:47:32 +0200

    HW 54 (105) on CW55

--- annotation-examples/src/main/java/telran/annotation/examples/TestClass.java
+++ annotation-examples/src/main/java/telran/annotation/examples/TestClass.java
@@ -4,29 +4,13 @@ import telran.test.annotation.BeforeEach;
 import telran.test.annotation.Test;
 
 public class TestClass {
-	@BeforeEach
-	void testBeforeEach ()
-	{
-		fBeforeEach();
-		System.out.println("Hi, I'm before each test1");
-	}
+	
 	private void f1(int b) { 
 		System.out.println("test f1 passed " + b);
 	}
 	private void f1() {
 		System.out.println("test f1 failed ");
 	}
-	
-	private void fBeforeEach() {
-		System.out.println("Hi, I'm before each test2");
-	}
-	/*
-	@Test
-	void test2() {
-		fBeforeEach();
-	}
-	*/
-	
 	
 	
 	@Test
@@ -36,6 +20,14 @@ public class TestClass {
 	@Test
 	void test() {
 		f1();
+	}
+	@BeforeEach
+	void setUp() {
+		System.out.println("set up");
+	}
+	@BeforeEach
+	void setUp2() {
+		System.out.println("set up2");
 	}
 	
 }
